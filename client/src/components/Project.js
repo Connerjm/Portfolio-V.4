526fa254ecb28c6dac7a7fd0db30325f9a4a205a
Conner Martin <connerjm2@gmail.com>
2021-05-11 02:11:46 -0700

    Filling out project component.

---
+++ client/src/components/Project.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Project(props)
+{
+    return (
+        <div className="card">
+            <div className="card-image">
+                <img src={props.image} alt="A small screenshot of the assosiated project." />
+            </div>
+            <div className="card-content">
+                <div className="content">
+                    <h4 className="title">{props.name}</h4>
+                    <p>{props.description}</p>
+                </div>
+            </div>
+            <footer className="card-footer">
+                <a href={props.url} target="_blank" class="card-footer-item">Repo</a>
+                {props.deployment ? <a href={props.deployment} target="_blank" class="card-footer-item">Deployment</a> : <p class="card-footer-item">N/A</p>}
+            </footer>
+        </div>
+    );
+}
