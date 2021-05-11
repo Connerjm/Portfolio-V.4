929758f6a1bd5c11ee883fb085da80dfa2d9be73
Conner Martin <connerjm2@gmail.com>
2021-05-10 17:30:31 -0700

    Adding basic files.

---
+++ server.js
@@ -0,0 +1,24 @@
+//Imports
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8008;
+const app = express();
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/build"));
+}
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res)
+{
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+//Start server.
+app.listen(PORT, function()
+{
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
