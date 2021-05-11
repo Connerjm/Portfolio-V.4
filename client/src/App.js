a3586f5d4e74b5242cb4a686c05991edb80004e4
Conner Martin <connerjm2@gmail.com>
2021-05-10 18:10:47 -0700

    Adding files for required components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-class App extends Component
+export default function App()
 {
-    render()
-    {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+    return (
+        <h1>Hello World</h1>
+    );
 }
-
-export default App;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-//Imports
+//Imports.
 const express = require("express");
 const path = require("path");
+
+//Set port and app.
 const PORT = process.env.PORT || 8008;
 const app = express();
 
